6feec3f401ba03e2c45212ac5ff68d5d97089527
MelonConYogurt <alejopsornal@gmail.com>
2025-02-23 22:33:42 -0500

    commit

--- src/testCycle.py
+++ src/testCycle.py
@@ -2,7 +2,5 @@ def cycles():
     for i in range (5):
         for j in range(5):
             print(f"El valor del primer ciclo es de (i): {i} | El valor del ciclo anidado es de (j) : {j}")
-    
-
 if __name__ == '__main__':
     cycles()
